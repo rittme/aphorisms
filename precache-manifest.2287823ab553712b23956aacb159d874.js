6114165c360fb1d5ada2f704544433df58e651b5
Bernardo Rittmeyer <bernardo@rittme.com>
2019-09-12 08:02:13 +0800

    Updates

---
+++ precache-manifest.2287823ab553712b23956aacb159d874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "346d9460c16705e5f0939cbd4f8b96cc",
+    "url": "/procrustes/index.html"
+  },
+  {
+    "revision": "aeb754107d2fd5989cf9",
+    "url": "/procrustes/static/css/main.22b34e69.chunk.css"
+  },
+  {
+    "revision": "78e75b5a2380301cd390",
+    "url": "/procrustes/static/js/2.44242e7d.chunk.js"
+  },
+  {
+    "revision": "aeb754107d2fd5989cf9",
+    "url": "/procrustes/static/js/main.3f32874b.chunk.js"
+  },
+  {
+    "revision": "35a60ef30ddd9a287d5c",
+    "url": "/procrustes/static/js/runtime~main.0b4078a7.js"
+  },
+  {
+    "revision": "a28c7df1faaa974ecf90c09f498834a2",
+    "url": "/procrustes/static/media/greek.a28c7df1.jpg"
+  }
+]);
